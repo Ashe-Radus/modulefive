d09a69b85027003c92058f25d6c007bd5e9681f2
Scoripo <aradusewicz@gmail.com>
2022-10-18 16:53:47 -0700

    add div for each hour, created var for each time, added moment

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,32 @@
 //this will hold user input and save it to the local storage. 
 
+var formEl = $('#schedule');
+var nineEl = $('input[name="nine"]');
+var tenEl = $('input[name="ten"]');
+var elevenEl = $('input[name="eleven"]');
+var twelveEl = $('input[name="twelve"]');
+var oneEl = $('input[name="one"]');
+var twoEl = $('input[name="two"]');
+var threeEl = $('input[name="three"]');
+var fourEl = $('input[name="four"]');
+var fiveEl = $('input[name="five"]');
+//var textEl =$('input[text]');
+
+function handleFormSubmit(event){
+event.preventDefault();
+
+var meetingItem = $('input[name="nine"]').val();
+
+console.log('nine:', nineEl.val());
+console.log('ten:', tenEl.val());
+}
+//localStorage.setItem('text');
+
+formEl.on('submit',handleFormSubmit);
+
+//borrowed from webfx for how to add moment js 
+(function() {
+    var NowMoment = moment();
+    var NowDate = new Date(); 
+    var eDisplayMoment = document.getElementById('displayMoment'); eDisplayMoment.innerHTML = NowMoment
+    var eDisplayDate = document.getElementById('displayJsDate'); eDisplayDate.innerHTML = NowDate; })(); 
